5b8ac83d735a1e7f659b737b0024eb4c17ce464e
ashish0077 <ashish007722@gmail.com>
2019-10-07 14:33:32 +0530

    RegisterActivity: SignUp now can create new users via email and password
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

--- app/src/main/java/com/example/letschat/RegisterActivity.java
+++ app/src/main/java/com/example/letschat/RegisterActivity.java
@@ -1,14 +1,67 @@
 package com.example.letschat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private static final String TAG = "RegisterActivity";
+
+    private TextInputLayout mDisplayName;
+    private TextInputLayout mEmail;
+    private TextInputLayout mPassword;
+    private Button mSignupBtn;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mDisplayName = (TextInputLayout) findViewById(R.id.reg_display_name_editText);
+        mEmail = (TextInputLayout) findViewById(R.id.reg_email_editText);
+        mPassword = (TextInputLayout) findViewById(R.id.reg_password_editText);
+        mSignupBtn = (Button) findViewById(R.id.reg_signup_btn);
+        mAuth = FirebaseAuth.getInstance();
+
+        mSignupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mEmail.getEditText().getText().toString();
+                String password = mPassword.getEditText().getText().toString();
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()) {
+                                    Log.d(TAG, "createUserWithEmail:success");
+                                    Toast.makeText(RegisterActivity.this, "Success",Toast.LENGTH_SHORT).show();
+                                    Intent mainActivty = new Intent(RegisterActivity.this, MainActivity.class);
+                                    startActivity(mainActivty);
+                                    finish();
+                                } else {
+                                    Log.e(TAG, "createUserWithEmail:failure");
+                                    Toast.makeText(RegisterActivity.this, "Failed, Authentication Problem",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
