22229837731348d1421c3e3b07a8aa6dc97713fb
ashish0077 <ashish007722@gmail.com>
2019-10-07 13:43:02 +0530

    Initial Commit MainActivity, StartActivity, RegisterActivity, Authentication Checking is on the mainActivity onStart()
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

---
+++ app/src/main/java/com/example/letschat/StartActivity.java
@@ -0,0 +1,31 @@
+package com.example.letschat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartActivity extends AppCompatActivity {
+
+    private Button mRegBtn;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        mRegBtn = (Button) findViewById(R.id.start_reg_btn);
+        mRegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerActivity = new Intent(StartActivity.this, RegisterActivity.class);
+                startActivity(registerActivity);
+            }
+        });
+    }
+
+
+}
