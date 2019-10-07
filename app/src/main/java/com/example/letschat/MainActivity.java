323befa4e90179babfe4142fe31444f1c63b2482
ashish0077 <ashish007722@gmail.com>
2019-10-07 15:45:35 +0530

    Logout Functionality and AppBar
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

--- app/src/main/java/com/example/letschat/MainActivity.java
+++ app/src/main/java/com/example/letschat/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.letschat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -11,6 +15,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Lets Chat");
     }
 
     @Override
@@ -27,9 +36,32 @@ public class MainActivity extends AppCompatActivity {
         //Check if user is signed in
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser == null) {
-            Intent startActivity = new Intent(MainActivity.this, StartActivity.class);
-            startActivity(startActivity);
-            finish();
+            sendToStart();
         }
+    }
+
+    private void sendToStart() {
+        Intent startActivity = new Intent(MainActivity.this, StartActivity.class);
+        startActivity(startActivity);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if(item.getItemId() == R.id.main_menu_item_logout) {
+            mAuth.signOut();
+            sendToStart();
+        }
+
+        return true;
     }
 }
