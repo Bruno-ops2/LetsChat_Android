6d6be39ffb4a596dfa0e6c4d4ab148289bd6c076
ashish0077 <ashish007722@gmail.com>
2019-10-09 03:02:07 +0530

    update SettingsActivity: fetching data from DB fetching data from DB and displaying it in the required fields
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

--- app/src/main/java/com/example/letschat/SettingsActivity.java
+++ app/src/main/java/com/example/letschat/SettingsActivity.java
@@ -1,14 +1,46 @@
 package com.example.letschat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private static final String TAG = "SettingsActivity";
+    private DatabaseReference mDatabase;
+    private TextView mStatus;
+    private TextView mName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        mStatus = (TextView) findViewById(R.id.settings_status_textView);
+        mName = (TextView) findViewById(R.id.settings_name_textView);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mName.setText(dataSnapshot.child("name").getValue().toString());
+                mStatus.setText(dataSnapshot.child("status").getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
