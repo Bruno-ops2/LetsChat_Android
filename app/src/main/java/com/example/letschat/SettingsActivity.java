b99d0d57bcd0d65937e4bcce5049726f98d4ff23
ashish0077 <ashish007722@gmail.com>
2019-10-10 04:33:42 +0530

    added dependency for Image Cropping Update SettingsActivity, onClickListener for change profile image button, created an intent for selecting image from device and starting CropImageActivity of the added library for croping the image, set the Aspect Ratio to 1:1 to make sure it is square
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

--- app/src/main/java/com/example/letschat/SettingsActivity.java
+++ app/src/main/java/com/example/letschat/SettingsActivity.java
@@ -1,14 +1,17 @@
 package com.example.letschat;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -17,14 +20,18 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.theartofdev.edmodo.cropper.CropImage;
 
 public class SettingsActivity extends AppCompatActivity {
 
     private static final String TAG = "SettingsActivity";
+    private static final int PICK_IMAGE_REQUEST = 1;
+
     private DatabaseReference mDatabase;
     private TextView mStatus;
     private TextView mName;
     private Button mStatusUpdateBtn;
+    private Button mChangeDpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +40,18 @@ public class SettingsActivity extends AppCompatActivity {
         mStatus = (TextView) findViewById(R.id.settings_status_textView);
         mName = (TextView) findViewById(R.id.settings_name_textView);
         mStatusUpdateBtn = (Button) findViewById(R.id.settings_change_status_button);
+        mChangeDpBtn = (Button) findViewById(R.id.settings_change_profile_button);
+
+        mChangeDpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Image"), PICK_IMAGE_REQUEST);
+
+            }
+        });
 
         mStatusUpdateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,5 +74,17 @@ public class SettingsActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && requestCode == PICK_IMAGE_REQUEST) {
+            Log.d(TAG, "Successfully Selected the Image");
+            Toast.makeText(this, "Got the Image", Toast.LENGTH_SHORT).show();
+            Uri uri = data.getData();
+
+            CropImage.activity(uri).setAspectRatio(1,1).start(this);
+        }
     }
 }
