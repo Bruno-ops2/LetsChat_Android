6ce6f1d1608a72f28f3e8890df7646d3982ed7c2
ashish0077 <ashish007722@gmail.com>
2019-10-09 16:01:25 +0530

    update StatusUpdateActivity fixed toolbar, added the Intent in SettingsActivity
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

--- app/src/main/java/com/example/letschat/SettingsActivity.java
+++ app/src/main/java/com/example/letschat/SettingsActivity.java
@@ -3,8 +3,11 @@ package com.example.letschat;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,6 +24,7 @@ public class SettingsActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
     private TextView mStatus;
     private TextView mName;
+    private Button mStatusUpdateBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,15 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         mStatus = (TextView) findViewById(R.id.settings_status_textView);
         mName = (TextView) findViewById(R.id.settings_name_textView);
+        mStatusUpdateBtn = (Button) findViewById(R.id.settings_change_status_button);
+
+        mStatusUpdateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent statusUpdateIntent = new Intent(SettingsActivity.this, StatusUpdateActivity.class);
+                startActivity(statusUpdateIntent);
+            }
+        });
 
         mDatabase = FirebaseDatabase.getInstance().getReference().child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
         mDatabase.addValueEventListener(new ValueEventListener() {
--- app/src/main/java/com/example/letschat/StatusUpdateActivity.java
+++ app/src/main/java/com/example/letschat/StatusUpdateActivity.java
@@ -1,14 +1,25 @@
 package com.example.letschat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class StatusUpdateActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private TextInputLayout mtextInputLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status_update);
-    }
+        mtextInputLayout = (TextInputLayout) findViewById(R.id.status_update_editText);
+        mToolbar = (Toolbar) findViewById(R.id.status_update_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Status Update");
+     }
 }
