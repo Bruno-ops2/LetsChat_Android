f1f2b4f94f4906b774b4849687cea1bb8c005751
ashish0077 <ashish007722@gmail.com>
2019-11-01 03:43:27 +0530

    Update ProfileActivity: fetching data from DB using UID
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

--- app/src/main/java/com/example/letschat/ProfileActivity.java
+++ app/src/main/java/com/example/letschat/ProfileActivity.java
@@ -1,5 +1,6 @@
 package com.example.letschat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,6 +10,14 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
 
@@ -20,6 +29,7 @@ public class ProfileActivity extends AppCompatActivity {
     private TextView status;
     private Button sendRequestBtn;
     private String UID;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,20 +38,34 @@ public class ProfileActivity extends AppCompatActivity {
         UID = getIntent().getExtras().getString("UserId");
         Log.d(TAG, "onCreate: UID : " + UID);
 
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(UID);
+
         //binding views
         profileImage = findViewById(R.id.profileActivity_profile_imageview);
         name = findViewById(R.id.profileActivity_name_textview);
         status = findViewById(R.id.profileActivity_status_textview);
         sendRequestBtn = findViewById(R.id.profileActivity_send_reques_button);
 
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                name.setText(dataSnapshot.child("name").getValue().toString());
+                status.setText(dataSnapshot.child("status").getValue().toString());
+                Picasso.get().load(dataSnapshot.child("profile_image").getValue().toString()).placeholder(R.drawable.avatar).into(profileImage);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(ProfileActivity.this, "Unable to fetch User Data", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
         //making status bar trasparent
         Window w = getWindow();
         w.setFlags(
                 WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
                 WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-
-
-
 
     }
 }
