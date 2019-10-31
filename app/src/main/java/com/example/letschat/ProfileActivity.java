276291d68bc02fa175ba6d57e24682ebd4bf793f
ashish0077 <ashish007722@gmail.com>
2019-11-01 03:02:07 +0530

    ProfileActivity layout updated
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

--- app/src/main/java/com/example/letschat/ProfileActivity.java
+++ app/src/main/java/com/example/letschat/ProfileActivity.java
@@ -3,6 +3,8 @@ package com.example.letschat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        //making status bar trasparent
+        Window w = getWindow();
+        w.setFlags(
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+
     }
 }
