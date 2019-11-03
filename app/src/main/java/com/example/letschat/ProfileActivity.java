acc8e9685ff89a7307d6025b826053fd1f69035a
ashish0077 <ashish007722@gmail.com>
2019-11-03 23:05:21 +0530

    FriendRequest sending and receiving now the users can send and receieve friend requests
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

--- app/src/main/java/com/example/letschat/ProfileActivity.java
+++ app/src/main/java/com/example/letschat/ProfileActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -13,6 +14,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,6 +27,8 @@ import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
 
+import java.sql.Struct;
+
 public class ProfileActivity extends AppCompatActivity {
 
     private static final String TAG = "ProfileActivity";
@@ -30,8 +37,12 @@ public class ProfileActivity extends AppCompatActivity {
     private TextView status;
     private Button sendRequestBtn;
     private String UID;
-    private DatabaseReference mDatabase;
+    private DatabaseReference mUsersDatabase;
+    private DatabaseReference mFriendReqDatabase;
     private ProgressDialog mProgressDialog;
+    private FirebaseUser mCurrentUser;
+
+    private String profileUserState = "notFriends";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +51,12 @@ public class ProfileActivity extends AppCompatActivity {
         UID = getIntent().getExtras().getString("UserId");
         Log.d(TAG, "onCreate: UID : " + UID);
 
+        //making status bar trasparent
+        Window w = getWindow();
+        w.setFlags(
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
         //progress dialog
         mProgressDialog = new ProgressDialog(this);
         mProgressDialog.setTitle("Loading User Data");
@@ -47,7 +64,10 @@ public class ProfileActivity extends AppCompatActivity {
         mProgressDialog.setCanceledOnTouchOutside(false);
         mProgressDialog.show();
 
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(UID);
+        mUsersDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(UID);
+        mFriendReqDatabase = FirebaseDatabase.getInstance().getReference().child("Friend_Request");
+        Log.d(TAG, "onCreate: " + mFriendReqDatabase.toString());
+        mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
 
         //binding views
         profileImage = findViewById(R.id.profileActivity_profile_imageview);
@@ -55,7 +75,7 @@ public class ProfileActivity extends AppCompatActivity {
         status = findViewById(R.id.profileActivity_status_textview);
         sendRequestBtn = findViewById(R.id.profileActivity_send_reques_button);
 
-        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+        mUsersDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 name.setText(dataSnapshot.child("name").getValue().toString());
@@ -72,11 +92,60 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
-        //making status bar trasparent
-        Window w = getWindow();
-        w.setFlags(
-                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
-                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        sendRequestBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                sendRequestBtn.setEnabled(false);
+                Log.d(TAG, "onClick: button disables");
+                final ProgressDialog progressDialog = new ProgressDialog(ProfileActivity.this);
+                progressDialog.setTitle("Sending Request");
+                progressDialog.setMessage("Please wait while the request is being sent");
+                progressDialog.setCanceledOnTouchOutside(false);
+                progressDialog.show();
+
+
+                //--------------------------NOT FRIENDS SECTION---------------------------------
+
+                if(profileUserState.equals("notFriends")) {
+
+                    Log.d(TAG, "onClick: inside not friends");
+
+                    mFriendReqDatabase
+                            .child(mCurrentUser.getUid())
+                            .child(UID)
+                            .child("request_type")
+                            .setValue("sent")
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+
+                                    if(task.isSuccessful()) {
+                                        mFriendReqDatabase
+                                                .child(UID)
+                                                .child(mCurrentUser.getUid())
+                                                .child("request_type")
+                                                .setValue("received");
+
+                                        profileUserState = "req_sent";
+                                        progressDialog.dismiss();
+
+                                    }
+
+                                    if (!task.isSuccessful()) {
+                                        Toast.makeText(ProfileActivity.this,
+                                                "Unable to send request, Try Agin",
+                                                Toast.LENGTH_SHORT).show();
+                                        sendRequestBtn.setEnabled(true);
+                                        profileUserState = "not_friends";
+                                        progressDialog.hide();
+                                    }
+                                }
+                            });
+
+                }
+            }
+        });
 
     }
 }
