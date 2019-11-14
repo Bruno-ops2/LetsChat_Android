915905394d4dcc2828596c3a54f3ab8d9446c3ce
ashish0077 <ashish007722@gmail.com>
2019-11-15 03:05:04 +0530

    LetsChat: Application.ActivityLifeCyclerCallbacks Now we can detect when the application is in foreground and when the application is in background and hence mark the online/lastSeen of the User
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

---
+++ app/src/main/java/com/example/letschat/LetsChat.kt
@@ -0,0 +1,124 @@
+package com.example.letschat
+
+import android.app.Activity
+import android.app.Application
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import java.lang.Exception
+import java.lang.NullPointerException
+import java.lang.ref.WeakReference
+import java.util.concurrent.atomic.AtomicBoolean
+import androidx.core.os.HandlerCompat.postDelayed
+
+
+
+class LetsChat : Application(), Application.ActivityLifecycleCallbacks {
+
+    private var currentActivityReference : WeakReference<Activity>? = null
+    private var mUserDatabase : DatabaseReference? = null
+    private val applicationBackgrounded = AtomicBoolean(true)
+    private val INTERVAL_BACKGROUND_STATE_CHANGE = 750L
+
+    override fun onCreate() {
+        super.onCreate()
+        registerActivityLifecycleCallbacks(this)
+
+        var mAuth = FirebaseAuth.getInstance()
+
+        try {
+            mUserDatabase = FirebaseDatabase.getInstance().reference.child("Users").child(mAuth.currentUser?.uid ?: throw NullPointerException())
+        } catch (e : Exception) {
+            mUserDatabase = null
+        }
+
+
+        /*mUserDatabase?.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(databaseError: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                mUserDatabase?.child("lastSeen")?.onDisconnect()?.setValue(false.toString())
+                *//*mUserDatabase.child("lastSeen").setValue(true)*//*
+
+            }
+        })*/
+
+    }
+
+    fun onEnterForeground() {
+        //This is where you'll handle logic you want to execute when your application enters the foreground
+        Log.d(this.javaClass.name, "activity is in foreground" )
+        if(mUserDatabase != null)
+            mUserDatabase?.child("lastSeen")?.setValue("true")
+
+    }
+
+    fun onEnterBackground() {
+        //This is where you'll handle logic you want to execute when your application enters the background
+        Log.d(this.javaClass.name, "activity is in background" )
+        if(mUserDatabase != null)
+            mUserDatabase?.child("lastSeen")?.setValue("false")
+
+    }
+
+    private fun determineForegroundStatus() {
+        if (applicationBackgrounded.get()) {
+            onEnterForeground()
+            applicationBackgrounded.set(false)
+        }
+    }
+
+    private fun determineBackgroundStatus() {
+        Handler().postDelayed({
+            if (!applicationBackgrounded.get() && currentActivityReference == null) {
+
+                applicationBackgrounded.set(true)
+                onEnterBackground()
+            }
+        }, INTERVAL_BACKGROUND_STATE_CHANGE)
+    }
+
+
+
+
+    //interface methods
+
+    override fun onActivityPaused(activity: Activity) {
+        currentActivityReference = null
+        determineBackgroundStatus()
+    }
+
+    override fun onActivityStarted(activity: Activity) {
+         //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onActivityDestroyed(activityp0: Activity) {
+         //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onActivitySaveInstanceState(activity: Activity, bundle: Bundle) {
+         //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onActivityStopped(activity: Activity) {
+         //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onActivityCreated(activity: Activity, bundle: Bundle?) {
+         //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onActivityResumed(activity: Activity) {
+
+        currentActivityReference = WeakReference(activity)
+        determineForegroundStatus()
+
+    }
+}
