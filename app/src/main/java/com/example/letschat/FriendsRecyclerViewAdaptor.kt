46d856483198090902fdeeb5df2717c5a6006850
ashish0077 <ashish007722@gmail.com>
2019-11-15 03:28:31 +0530

    Update FriendsRecyclerViewAdaptor
    Signed-off-by: ashish0077 <ashish007722@gmail.com>

--- app/src/main/java/com/example/letschat/FriendsRecyclerViewAdaptor.kt
+++ app/src/main/java/com/example/letschat/FriendsRecyclerViewAdaptor.kt
@@ -17,19 +17,29 @@ class FriendsRecyclerViewAdaptor(
     ) : RecyclerView.Adapter<FriendsRecyclerViewAdaptor.ViewHolder>() {
 
     val TAG: String = "FriendsAdaptor"
-    
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder (
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        Log.d(TAG, "onCreateViewHolder called")
+        return ViewHolder(LayoutInflater.from(mContext).inflate(R.layout.single_friend, parent, false))
+    }
+
+    /*override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder (
             LayoutInflater.from(mContext).inflate(R.layout.single_friend, parent, false)
-    ).also { Log.d(TAG, "onCreateViewHolder called") }
+    ).also { Log.d(TAG, "onCreateViewHolder called") }*/
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         Log.d(TAG, "onBindViewHolder called")
         val name = friendsDatabaseResponse[position].name
         val profileImage = friendsDatabaseResponse[position].profileImage
+        var lastSeen = friendsDatabaseResponse[position].lastSeen
         Log.d(TAG, "view holder name ; $name")
         Log.d(TAG, "view holder name ; $profileImage")
 
         holder.name.text = name
+        when(lastSeen) {
+            "true" -> holder.lastSeen.text = "Online"
+            "false" -> holder.lastSeen.text = "Offline"
+        }
         Picasso.get().load(profileImage).placeholder(R.drawable.avatar).into(holder.profileImage)
     }
 
